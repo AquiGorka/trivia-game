4fe3c8ab6168a70e6ec0d499c367974e3b0ad721
Gorka <gorka@aquigorka.com>
2018-07-14 14:07:56 +0200

    Components: update index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
-export { View as GameView } from './Game/Game';
+export { Store as AnswersStore } from './Answers/Answers';
+export { View as GameView, Store as GameStore } from './Game/Game';
+export { Store as QuestionsStore } from './Questions/Questions';
 export { View as ResultsView } from './Results/Results';
 export { View as WelcomeView } from './Welcome/Welcome';
