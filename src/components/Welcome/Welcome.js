dfd9d3e32e2a26e64d2a193ba183b5adb97b161d
Gorka <gorka@aquigorka.com>
2018-07-14 15:19:20 +0200

    Welcome: add wireframe css rules

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { GAME } from '../Game/Game';
+import './Welcome.css';
 
 const View = props => {
   const { mode, flux, questionsLoading, questionsError } = props;
@@ -9,14 +10,34 @@ const View = props => {
   }
 
   return (
-    <div>
-      <h2>Welcome to the Trivia Challenge</h2>
-      <div>You will be presented with 10 True or False questions</div>
-      <div>Can you score 100%?</div>
-      <button disabled={!!questionsError || questionsLoading} onClick={() => flux({ mode: GAME })}>Begin</button>
-      {questionsLoading && <div>Loading questions</div>}
-      {questionsError}
-    </div>
+    <Fragment>
+      <header>
+        <h2>Welcome to the Trivia Challenge!</h2>
+      </header>
+      <main className="Welcome">
+        <div className="Welcome__body">
+          <h3>You will be presented with 10 True or False questions</h3>
+          <div>Can you score 100%?</div>
+        </div>
+        <div className="Welcome__footer">
+          {questionsLoading && (
+            <div className="Welcome__loading">Loading questions...</div>
+          )}
+          {!!questionsError && (
+            <div className="Welcome__error">{questionsError}</div>
+          )}
+          {!questionsError &&
+            !questionsLoading && (
+              <button
+                disabled={!!questionsError || questionsLoading}
+                onClick={() => flux({ mode: GAME })}
+              >
+                Begin
+              </button>
+            )}
+        </div>
+      </main>
+    </Fragment>
   );
 };
 
