0f223c716c60ab1356661ca26eda15345904f867
Gorka <gorka@aquigorka.com>
2018-07-14 14:06:27 +0200

    Welcome: add wireframse flow

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { GAME } from '../Game/Game';
 
 const View = props => {
-  const { mode } = props;
+  const { mode, flux, questionsLoading, questionsError } = props;
 
   if (mode) {
     return null;
   }
 
-  return 'Welcome';
+  return (
+    <div>
+      <h2>Welcome to the Trivia Challenge</h2>
+      <div>You will be presented with 10 True or False questions</div>
+      <div>Can you score 100%?</div>
+      <button disabled={!!questionsError || questionsLoading} onClick={() => flux({ mode: GAME })}>Begin</button>
+      {questionsLoading && <div>Loading questions</div>}
+      {questionsError}
+    </div>
+  );
 };
 
 export { View };
