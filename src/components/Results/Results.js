523b2f8abb17ba854db796622ae55b48e5bdf2fb
Gorka <gorka@aquigorka.com>
2018-07-14 14:06:42 +0200

    Results: add wireframes flow

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const RESULTS = 'results';
 
 const View = props => {
-  const { mode } = props;
+  const { mode, answers = [], questions = [], gameRestart } = props;
 
   if (mode !== RESULTS) {
     return null;
   }
 
-  return 'Results';
+  return (
+    <Fragment>
+      <div>You scored</div>
+      <div>
+        {answers.reduce((p, c) => (c.correct ? p + 1 : p), 0)}/{
+          questions.length
+        }
+      </div>
+      <ul>
+        {answers.map(({ question, correct }, index) => {
+          return (
+            <li key={`results-${index}`}>
+              <div>{correct ? 'Bien' : 'Mal'}</div>
+              <div>{question}</div>
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={gameRestart}>Play again?</button>
+    </Fragment>
+  );
 };
 
-export { View };
+export { RESULTS, View };
